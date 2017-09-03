552b01da2190dde6ad2933de53cf5cf3ca017a40
Sky Morey <smorey@degdigital.com>
2017-09-03 17:27:57 -0500

    updates

--- _build.cmd
+++ _build.cmd
@@ -1,13 +1,14 @@
 @echo off
-rm -rf _build
+rd /s /q _build
+rem rm -rf _build
 mkdir _build
 pushd _build
 
 mkdir build32 & pushd build32
-cmake -G "Visual Studio 12 2013" -DCMAKE_CUDA_FLAGS="-arch=sm_35" ../../src
+cmake -G "Visual Studio 11 2012" -DCMAKE_CUDA_FLAGS="-arch=sm_35" ../../src
 popd
 rem mkdir build64 & pushd build64
-rem cmake -G "Visual Studio 12 2013 Win64" -DCMAKE_CUDA_FLAGS="-arch=sm_35" ../../src
+rem cmake -G "Visual Studio 11 2012 Win64" -DCMAKE_CUDA_FLAGS="-arch=sm_35" ../../src
 rem popd
 cmake --build build32 --config Release
 rem cmake --build build64 --config Debug
