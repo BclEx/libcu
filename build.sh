fc722c99325294767ce9b1f00c3ef02bbe68c3e4
Jordan Anderson <jordan.anders@gmail.com>
2017-09-07 00:37:35 -0500

    Adding initial linux build script.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+rm -rf _build
+mkdir _build
+cd _build
+
+mkdir build32
+cd build32
+cmake -DCMAKE_CUDA_FLAGS="-arch=sm_35" ../../src
+cd ..
+
+cmake --build build32 --config Debug
+
+cd build32
+ctest -C Debug
+
+cd ../..
