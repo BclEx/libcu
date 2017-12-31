352b9765826d841fabdb4ccbac480d1d71973886
Sky Morey <smorey@degdigital.com>
2017-12-30 19:52:04 -0600

    updates

--- build.sh
+++ build.sh
@@ -1,21 +1,11 @@
 #! /bin/sh
 cp src/CMakeLists.work.txt src/CMakeLists.txt
 
+# START
 rm -rf _build
 mkdir _build
 cd _build
 
-# BUILD32
-mkdir build32
-cd build32
-cmake -DX64=0 -DCMAKE_CUDA_FLAGS="-arch=sm_60" ../../src
-cd ..
-
-cmake --build build32 --config Debug
-
-cd build32
-#ctest -C Debug
-cd ..
 
 # BUILD64
 mkdir build64
@@ -24,9 +14,22 @@ cmake -DX64=1 -DCMAKE_CUDA_FLAGS="-arch=sm_60" ../../src
 cd ..
 
 cmake --build build64 --config Debug
-
 cd build64
-#ctest -C Debug
+ctest -C Debug
 cd ..
 
+
+# BUILD32
+mkdir build32
+cd build32
+cmake -DX64=0 -DCMAKE_CUDA_FLAGS="-arch=sm_60" ../../src
+cd ..
+
+cmake --build build32 --config Debug
+cd build32
+ctest -C Debug
+cd ..
+
+
+# END
 cd ..
